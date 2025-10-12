5fb27e5a23762bed8c3918d56faf01cc43cb73e0
vincentversluis <vincentversluis@hotmail.com>
2025-10-12 15:55:01 +0200

    Tidying

--- projects/match_festivals/1_visualise_twins.py
+++ projects/match_festivals/1_visualise_twins.py
@@ -1,6 +1,5 @@
 # %% HEADER
 # Visualis the twinning of bands at festivals
-# TODO: ruff
 
 # %% IMPORTS
 from collections import Counter, defaultdict
@@ -15,41 +14,39 @@ import seaborn as sns
 
 # %% INPUTS
 artist_of_interest = "Insomnium"
-top_n = 25 
-genre_of_interest = ''  # 'Melodic Death Metal'
+top_n = 25
+genre_of_interest = ""  # 'Melodic Death Metal'
 
 # %% GET DATA
 # Load data
 with open("../../data/festivals.json", encoding="utf-8") as f:
     festivals = json.load(f)
-    
+
 # Only keep festivals with more than one artist
 festivals = [festival for festival in festivals if len(festival["artists"]) > 1]
 
 # %% ANALYSE
 # Get artists and their genres
-artists = chain.from_iterable([festival['artists'] for festival in festivals])
-artist_genres = {artist['name']: artist['genre'] for artist in artists}
+artists = chain.from_iterable([festival["artists"] for festival in festivals])
+artist_genres = {artist["name"]: artist["genre"] for artist in artists}
 
 # Get top so many performers at festivals
 top_performers = Counter(
-    chain.from_iterable(
+    chain.from_iterable([
         [
-            [
-                artist['name'] 
-                for artist in festival['artists']
-                if 
-                    # If genres of interests are defined, filter by genre
-                    not genre_of_interest 
-                    or artist['genre'] == genre_of_interest
-            ] 
-            for festival 
-            in festivals
-        ]))
+            artist["name"]
+            for artist in festival["artists"]
+            if
+            # If genres of interests are defined, filter by genre
+            not genre_of_interest or artist["genre"] == genre_of_interest
+        ]
+        for festival in festivals
+    ])
+)
 
 # Collect artists that have performed at the same festival (festival twins), as long
 # as one of the artists is in the genre of interest
-festival_twins = defaultdict(lambda: {'festivals': [], 'count': 0})
+festival_twins = defaultdict(lambda: {"festivals": [], "count": 0})
 for festival in festivals:
     for artist in festival["artists"]:
         for other_artist in festival["artists"]:
@@ -62,16 +59,15 @@ for festival in festivals:
                     continue
             # Add alphabetically ordered pairs to avoid reciprocal pairs
             if artist["name"] < other_artist["name"]:
-                artists = (artist['name'], other_artist['name'])
+                artists = (artist["name"], other_artist["name"])
                 festival_twins[artists]["festivals"].append(festival["name"])
                 festival_twins[artists]["count"] += 1
 
 # Get only artists that have performed at more than one festival and sort
 top_festival_twins = {
-    artists: festivals['count']
-    for artists, festivals 
-    in festival_twins.items()
-    if festivals['count'] > 1
+    artists: festivals["count"]
+    for artists, festivals in festival_twins.items()
+    if festivals["count"] > 1
 }
 
 # %% VISUALISE
@@ -93,17 +89,24 @@ mask = np.eye(len(matrix), dtype=bool)
 
 # Plot
 plt.figure(figsize=(10, 8))
-sns.heatmap(matrix, mask=mask, annot=True, fmt='d',
-            cmap='Blues', linewidths=0.5, linecolor='grey')
+sns.heatmap(
+    matrix,
+    mask=mask,
+    annot=True,
+    fmt="d",
+    cmap="Blues",
+    linewidths=0.5,
+    linecolor="grey",
+)
 
 # Force integer ticks on colorbar
 cbar = plt.gca().collections[0].colorbar
 cbar.ax.yaxis.set_major_locator(ticker.MaxNLocator(integer=True))
 
 # Labels and layout
-plt.title('Shared lineups between top performing artists between 2022 and 2025')
-plt.xticks(rotation=45, fontname='DejaVu Sans Mono')
-plt.yticks(rotation=0, fontname='DejaVu Sans Mono')
+plt.title("Shared lineups between top performing artists between 2022 and 2025")
+plt.xticks(rotation=45, fontname="DejaVu Sans Mono")
+plt.yticks(rotation=0, fontname="DejaVu Sans Mono")
 plt.tight_layout()
 plt.show()
 
@@ -117,44 +120,50 @@ counts = [count for _, count in top_performers_sorted]
 
 # Plot
 plt.figure(figsize=(10, 0.25 * len(labels)))
-plt.barh(labels, counts, color='#1f77b4')
-plt.grid(axis='x', color='grey', linestyle='--', linewidth=0.5)
-plt.xlabel('Number of appearances')
-plt.title(f'Top performing {genre_of_interest.lower()} artists between 2022 and 2025')
+plt.barh(labels, counts, color="#1f77b4")
+plt.grid(axis="x", color="grey", linestyle="--", linewidth=0.5)
+plt.xlabel("Number of appearances")
+plt.title(f"Top performing {genre_of_interest.lower()} artists between 2022 and 2025")
 plt.gca().invert_yaxis()
 ax = plt.gca()
 ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
 
 # Monospaced font for nice alignment
-plt.xticks(fontname='DejaVu Sans Mono')
-plt.yticks(fontname='DejaVu Sans Mono')
+plt.xticks(fontname="DejaVu Sans Mono")
+plt.yticks(fontname="DejaVu Sans Mono")
 
 plt.tight_layout()
 plt.show()
 
 # %%
 ##### Top festival twins in genre of interest #####
-top_festival_twins_sorted = sorted(top_festival_twins.items(), key=lambda x: x[1], reverse=True)[:top_n]
+top_festival_twins_sorted = sorted(
+    top_festival_twins.items(), key=lambda x: x[1], reverse=True
+)[:top_n]
 
 # Format labels with centered ampersand and left/right aling
 artists = chain.from_iterable([artists for artists, count in top_festival_twins_sorted])
 name_chars = max([len(artist) for artist in artists])
-labels = [f"{a:>{name_chars}} & {b:<{name_chars}}" for (a, b), _ in top_festival_twins_sorted]
+labels = [
+    f"{a:>{name_chars}} & {b:<{name_chars}}" for (a, b), _ in top_festival_twins_sorted
+]
 counts = [count for _, count in top_festival_twins_sorted]
 
 # Plot
 plt.figure(figsize=(10, 0.25 * len(labels)))
-plt.barh(labels, counts, color='#1f77b4')
-plt.grid(axis='x', color='grey', linestyle='--', linewidth=0.5)
-plt.xlabel('Number of appearances')
-plt.title(f'Top artists sharing a lineup with a {genre_of_interest.lower()} artist between 2022 and 2025')
+plt.barh(labels, counts, color="#1f77b4")
+plt.grid(axis="x", color="grey", linestyle="--", linewidth=0.5)
+plt.xlabel("Number of appearances")
+plt.title(
+    f"Top artists sharing a lineup with a {genre_of_interest.lower()} artist between 2022 and 2025"
+)
 plt.gca().invert_yaxis()
 ax = plt.gca()
 ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
 
 # Use monospaced font for nice alignment
-plt.xticks(fontname='DejaVu Sans Mono')
-plt.yticks(fontname='DejaVu Sans Mono')
+plt.xticks(fontname="DejaVu Sans Mono")
+plt.yticks(fontname="DejaVu Sans Mono")
 
 plt.tight_layout()
 plt.show()
@@ -162,7 +171,9 @@ plt.show()
 # %%
 ##### Top twins with artist of interest #####
 # Sort and filter
-top_festival_twins_sorted = sorted(top_festival_twins.items(), key=lambda x: x[1], reverse=True)
+top_festival_twins_sorted = sorted(
+    top_festival_twins.items(), key=lambda x: x[1], reverse=True
+)
 top_festival_twins_filtered = [
     (other_artist, count)
     for (a, b), count in top_festival_twins_sorted
@@ -180,17 +191,19 @@ counts = [count for _, count in top_festival_twins_filtered]
 
 # Plot
 plt.figure(figsize=(10, 0.25 * len(labels)))
-plt.barh(labels, counts, color='#1f77b4')
-plt.grid(axis='x', color='grey', linestyle='--', linewidth=0.5)
-plt.xlabel('Number of Shared Festivals')
-plt.title(f'Top artists sharing a line-up with {artist_of_interest} at between 2022 and 2025')
+plt.barh(labels, counts, color="#1f77b4")
+plt.grid(axis="x", color="grey", linestyle="--", linewidth=0.5)
+plt.xlabel("Number of Shared Festivals")
+plt.title(
+    f"Top artists sharing a line-up with {artist_of_interest} at between 2022 and 2025"
+)
 plt.gca().invert_yaxis()
 ax = plt.gca()
 ax.xaxis.set_major_locator(ticker.MaxNLocator(integer=True))
 
 # Monospaced font for nice alignment
-plt.xticks(fontname='DejaVu Sans Mono')
-plt.yticks(fontname='DejaVu Sans Mono')
+plt.xticks(fontname="DejaVu Sans Mono")
+plt.yticks(fontname="DejaVu Sans Mono")
 
 plt.tight_layout()
 plt.show()
